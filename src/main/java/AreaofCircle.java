043d58cb6c5842928d7b59c403f2ccf6f794f5f4
Gowthami Beerakam <96508604+B-Gowthami@users.noreply.github.com>
2022-01-11 06:55:02 +0530

    Update AreaofCircle.java

--- src/main/java/AreaofCircle.java
+++ src/main/java/AreaofCircle.java
@@ -7,11 +7,14 @@ class AreaOfCircle {
         System.out.println("Area of the circle is ::" + area);
     }
 }
-class Square extends AreaOfCircle{
-    public static void AreaOfSquare(int radius){
+public static double area(int base,int height){
         double area;
-        System.out.println("Enter the side of the Square ::"+radius);
-        area=radius*radius;
-        System.out.println("Area of the square is ::" + area);
+        area=0.5*base*height;
+        System.out.println("Area of the Triangle is ::" + area);
+        return area;
     }
-}
+    public static double area(double length,double width){
+        double area=length*width;
+        System.out.println("Area of rectangle:"+area);
+        return area;
+    }
