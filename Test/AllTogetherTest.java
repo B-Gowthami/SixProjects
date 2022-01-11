dbb31a7c5970041f6bd5239188cfad16d0fbf2bb
B-Gowthami <beerakamgowthami@gmail.com>
2022-01-11 07:35:26 +0530

    Commit

---
+++ Test/AllTogetherTest.java
@@ -0,0 +1,61 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+import java.time.*;
+import static java.time.LocalTime.now;
+import static org.junit.Assert.*;
+public class AllTogetherTest {
+    BodyMassIndex BMI = new BodyMassIndex();
+    @Test
+    public void bodyMassIndexTest1 () {
+        TestCase.assertEquals(18.52, BMI.bodyMassIndex());
+    }
+    @Test
+    public void bodyMassIndexTest2 () {
+        assertNotNull(BMI.bodyMassIndex());
+    }
+
+    BiggestNumber number = new BiggestNumber();
+    @Test
+    public void biggestNumberTest1 () {
+        assertEquals(11, number.biggestNumber());
+    }
+    @Test
+    public void biggestNumberTest2 () {
+        assertTrue(number.biggestNumber() != 10);
+    }
+    ArmstrongNumber armstrongNumber = new ArmstrongNumber();
+    @Test
+    public void armstrongNumberTest() {
+        TestCase.assertEquals(1634, armstrongNumber.armstrongNumber());
+    }
+
+    @Test
+    public void armstrongNumberTest1() {
+        TestCase.assertNotSame(568, armstrongNumber.armstrongNumber());
+    }
+    UTCDateTime UTC=new UTCDateTime();
+    @Test
+    public void UTCTest() {
+        try {
+            assertEquals(UTC.UTC(),
+                    now().atDate(LocalDate.now(ZoneId.from(LocalDateTime.now().minusSeconds(19800)))));
+        }
+        catch (DateTimeException ex) {
+            ex.printStackTrace();
+        }
+    }
+    AreaOfCircle a = new AreaOfCircle();
+    @Test
+    public void areaOfCircleTest() {
+        TestCase.assertEquals(12.56,a.area(2));
+    }
+    @Test
+    public void areaOfTriangleTest() {
+        assertNotSame(10.0, a.area(2,4));
+    }
+    @Test
+    public void areaOfRectangleTest() {
+        assertNotNull("NotNull",a.area(2.0,2))
+    }
+
+}
